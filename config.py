b75eadc5357317ff1fd7fba6c44c4ad38d4c6d91
Nick Steinhauser <nicksteinhauser@MacBook-Air.local>
2017-11-24 12:20:19 -0600

    bare bones

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# Twitter API Keys
+consumer_key = "iR3o6kTmOCfhwNhpuIDTJdoam"
+consumer_secret = "ZprxzUK33Muyui1jGagPlwBq4MThDhR8NC7iimcF22N3ivM32M"
+access_token = "913516671574216704-QVr5dfL3PG6oum5OzF8Yfjawd0CD75g"
+access_token_secret = "5yHpDd8da7dLkAMUqWLZvR4bm4Od0Ul6YujPanGeZSWOK"
